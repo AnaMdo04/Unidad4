806d8dbbabef195881570f82e2bce13e1190034f
Ana Montes de Ory <amonory327@g.educaand.es>
2023-11-26 18:50:46 +0100

    Obtenemos y actualizamos un solo coche por su ID y la del concesionario

--- index.js
+++ index.js
@@ -91,3 +91,19 @@ app.post("/concesionarios/:id/coches", (request, response) => {
   concesionarios[id].coches.push(request.body);
   response.json({ message: "ok" });
 });
+
+// Obtenemos un solo coche de un concesionario por su ID y el ID del coche
+app.get("/concesionarios/:id/coches/:cocheId", (request, response) => {
+  const id = request.params.id;
+  const cocheId = request.params.cocheId;
+  const coche = concesionarios[id].coches[cocheId];
+  response.json({ coche });
+});
+
+// Actualizamos un coche de un concesionario por su ID y el ID del coche
+app.put("/concesionarios/:id/coches/:cocheId", (request, response) => {
+  const id = request.params.id;
+  const cocheId = request.params.cocheId;
+  concesionarios[id].coches[cocheId] = request.body;
+  response.json({ message: "ok" });
+});
