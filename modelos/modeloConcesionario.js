fbfd9844505431f6adf535a592151ce1f7bdb0ba
Ana Montes de Ory <amonory327@g.educaand.es>
2023-12-04 19:18:13 +0100

    Comentarios añadidos

--- modelos/modeloConcesionario.js
+++ modelos/modeloConcesionario.js
@@ -1,17 +1,22 @@
+// Importamos la biblioteca mongoose
 const mongoose = require("mongoose");
 
+// Definimos el esquema para el modelo de coche
 const cocheSchema = new mongoose.Schema({
   modelo: String,
   cv: Number,
   precio: Number,
 });
 
+// Definimos el esquema para el modelo de concesionario, que incluye una lista de coches
 const concesionarioSchema = new mongoose.Schema({
   nombre: String,
   direccion: String,
   coches: [cocheSchema],
 });
 
-// Exporta el modelo Concesionario
+// Creamos el modelo Concesionario a partir del esquema concesionarioSchema
 const Concesionario = mongoose.model("Concesionario", concesionarioSchema);
+
+// Exportamos el modelo Concesionario para que pueda ser utilizado en otros archivos
 module.exports = Concesionario;
