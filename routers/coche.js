964cd72fe81c068b16af66908f17bdc806d9ba39
Ana Montes de Ory <amonory327@g.educaand.es>
2023-12-10 19:52:23 +0100

    Mejoras del código

--- routers/coche.js
+++ routers/coche.js
@@ -1,14 +1,10 @@
 const express = require("express");
 const router = express.Router();
-
-// Importamos el modelo Coche desde la carpeta "modelos"
 const Coche = require("../modelos/modeloConcesionario");
 
-// Ruta para obtener todos los coches de un concesionario por su ID
 router.get("/:id/coches", async (req, res) => {
   try {
     const idConcesionario = req.params.id;
-    // Utilizamos el modelo Coche para obtener todos los coches de un concesionario específico
     const coches = await Coche.find({ concesionario: idConcesionario });
     res.json(coches);
   } catch (error) {
@@ -16,56 +12,57 @@ router.get("/:id/coches", async (req, res) => {
   }
 });
 
-// Ruta para obtener un solo coche de un concesionario por su ID y el ID del coche
 router.get("/:id/coches/:cocheId", async (req, res) => {
   try {
     const idConcesionario = req.params.id;
     const cocheId = req.params.cocheId;
-    // Utilizamos el modelo Coche para obtener un coche específico de un concesionario
     const coche = await Coche.findOne({ _id: cocheId, concesionario: idConcesionario });
+    if (!coche) {
+      return res.status(404).json({ error: "Coche no encontrado" });
+    }
     res.json({ coche });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
 
-// Ruta para crear un nuevo coche en un concesionario
 router.post("/:id/coches", async (req, res) => {
   try {
     const idConcesionario = req.params.id;
-    // Añadimos el ID del concesionario al cuerpo de la solicitud antes de crear el nuevo coche
     req.body.concesionario = idConcesionario;
-    // Creamos una nueva instancia del modelo Coche con los datos del cuerpo de la solicitud
     const nuevoCoche = new Coche(req.body);
-    // Guardamos el nuevo coche en la base de datos
     await nuevoCoche.save();
-    res.json({ message: "Coche creado correctamente" });
+    res.status(201).json({ message: "Coche creado correctamente" });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
 
-// Ruta para actualizar un coche de un concesionario por su ID y el ID del coche
 router.put("/:id/coches/:cocheId", async (req, res) => {
   try {
     const idConcesionario = req.params.id;
     const cocheId = req.params.cocheId;
-    // Utilizamos el modelo Coche para actualizar un coche específico de un concesionario
-    const resultado = await Coche.updateOne({ _id: cocheId, concesionario: idConcesionario }, req.body);
-    res.json({ message: "ok" });
+    const resultado = await Coche.findByIdAndUpdate({ _id: cocheId, concesionario: idConcesionario }, req.body, {
+      new: true,
+    });
+    if (!resultado) {
+      return res.status(404).json({ error: "Coche no encontrado" });
+    }
+    res.json({ message: "Coche actualizado correctamente" });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
 
-// Ruta para borrar un coche de un concesionario por su ID y el ID del coche
 router.delete("/:id/coches/:cocheId", async (req, res) => {
   try {
     const idConcesionario = req.params.id;
     const cocheId = req.params.cocheId;
-    // Utilizamos el modelo Coche para borrar un coche específico de un concesionario
     const resultado = await Coche.deleteOne({ _id: cocheId, concesionario: idConcesionario });
-    res.json({ message: "ok" });
+    if (resultado.deletedCount === 0) {
+      return res.status(404).json({ error: "Coche no encontrado" });
+    }
+    res.json({ message: "Coche borrado correctamente" });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
--- routers/concesionario.js
+++ routers/concesionario.js
@@ -1,13 +1,9 @@
 const express = require("express");
 const router = express.Router();
-
-// Importamos el modelo Concesionario desde la carpeta "modelos"
 const Concesionario = require("../modelos/modeloConcesionario");
 
-// Ruta para obtener todos los concesionarios
 router.get("/", async (req, res) => {
   try {
-    // Utilizamos el modelo Concesionario para obtener todos los concesionarios de la base de datos
     const concesionarios = await Concesionario.find();
     res.json(concesionarios);
   } catch (error) {
@@ -15,11 +11,9 @@ router.get("/", async (req, res) => {
   }
 });
 
-// Ruta para obtener un solo concesionario por su ID
 router.get("/:id", async (req, res) => {
   try {
     const idConcesionario = req.params.id;
-    // Utilizamos el modelo Concesionario para obtener un concesionario específico de la base de datos
     const concesionario = await Concesionario.findById(idConcesionario);
     res.json(concesionario);
   } catch (error) {
@@ -27,36 +21,29 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-// Ruta para crear un nuevo concesionario
 router.post("/", async (req, res) => {
   try {
-    // Creamos una nueva instancia del modelo Concesionario con los datos del cuerpo de la solicitud
     const nuevoConcesionario = new Concesionario(req.body);
-    // Guardamos el nuevo concesionario en la base de datos
     await nuevoConcesionario.save();
-    res.json({ message: "Concesionario creado correctamente" });
+    res.status(201).json({ message: "Concesionario creado correctamente" });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
 
-// Ruta para actualizar un concesionario por su ID
 router.put("/:id", async (req, res) => {
   try {
     const idConcesionario = req.params.id;
-    // Utilizamos el modelo Concesionario para actualizar un concesionario específico de la base de datos
-    await Concesionario.findByIdAndUpdate(idConcesionario, req.body);
-    res.json({ message: "ok" });
+    await Concesionario.findByIdAndUpdate(idConcesionario, req.body, { new: true });
+    res.json({ message: "Concesionario actualizado correctamente" });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
 
-// Ruta para borrar un concesionario por su ID
 router.delete("/:id", async (req, res) => {
   try {
     const idConcesionario = req.params.id;
-    // Utilizamos el modelo Concesionario para borrar un concesionario específico de la base de datos
     await Concesionario.findByIdAndDelete(idConcesionario);
     res.json({ message: "Concesionario borrado correctamente" });
   } catch (error) {
